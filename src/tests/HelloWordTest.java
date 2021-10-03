bd93ff49c0b8ce93d8e5fa5b24a7fd99b8689a78
krigora <ihor.kryvynskyy@icloud.com>
2021-10-03 15:53:09 -0500

    updating Hello word class

--- src/tests/HelloWordTest.java
+++ src/tests/HelloWordTest.java
@@ -4,5 +4,7 @@ public class HelloWordTest {
 
     public static void main(String[] args) {
         System.out.println("Hello word");
+
+        System.out.println("one more hello world");
     }
 }
