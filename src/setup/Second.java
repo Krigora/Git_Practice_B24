5672ecb4066619f4f30d44186132622971a9f244
krigora <ihor.kryvynskyy@icloud.com>
2021-10-03 11:24:04 -0500

    updated second file again

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -3,13 +3,14 @@ package setup;
 public class Second {
 
     public void m(){
-
+        System.out.println("First method");
     }
     public  void m(int a){
-
+        System.out.println("Second method");
     }
 
     public void m(String s){
+        System.out.println("third method");
 
     }
 
